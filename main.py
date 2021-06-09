157964ff172023f5ba1763117e868460841bbcfc
PawelLawrynowicz <procyon97@gmail.com>
2021-06-09 19:22:08 +0200

    siema

--- main.py
+++ main.py
@@ -1,56 +1,134 @@
+from scipy.stats import rankdata
+from sklearn.metrics import accuracy_score
+from sklearn.base import clone
 from sklearn.datasets import make_classification
 import numpy as np
-from sklearn.feature_selection import f_classif
-from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif, SelectKBest
 from AnovaTransformer import AnovaFTransformer
-
+from sklearn.decomposition import PCA
+from sklearn.model_selection import StratifiedKFold
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 np.set_printoptions(suppress=True)
 X, y = make_classification(
-    n_samples=1000,
-    n_classes=2,
+    n_samples=20,
+    n_classes=3,
     n_features=20,
     n_redundant=4,
-    n_informative=5,
+    n_informative=2,
     random_state=1234,
+    n_clusters_per_class=1,
 )
 
+skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=1234)
 
-def calculate_f_value(X, y=None):
-    X0 = X[y == 0]
-    X1 = X[y == 1]
-    mean_of_total = np.mean(X)
-    mean_0 = np.mean(X0)
-    mean_1 = np.mean(X1)
-    total_ss = np.sum((X - mean_of_total) ** 2)  # sum of squares of all classes
-    within_ss = np.sum((X0 - mean_0) ** 2) + np.sum((X1 - mean_1) ** 2)  # sum of squares within classes
-    if len(X0) == len(X1):
-        between_ss = ((mean_0 - mean_of_total) ** 2 + (mean_1 - mean_of_total) ** 2) * len(
-            X0)  # sum of squares between classes
-    p_w = len(X) - len(np.unique(y))  # degrees of freedom - denominator
-    p_b = len(np.unique(y)) - 1  # degrees of freedom - numerator
-    if 'between_ss' in locals():
-        F = (between_ss / p_b) / (
-                within_ss / p_w)  # only when it is possible to calculate sum of squares between classes
-    else:
-        F = ((total_ss - within_ss) / p_b) / (within_ss / p_w)
-    return F
+clfs = {
+    "GNB": GaussianNB(),
+    "kNN": KNeighborsClassifier(),
+    "CART": DecisionTreeClassifier(random_state=7777),
+}
+
+# ds = {'anova': AnovaFTransformer().fit_transform(X, y, n_features=3), 'skb': SelectKBest(
+
+folds = 5
+# scores = np.zeros((len(clfs), len(ds), folds))
+# for idx, (data_id, dataset) in enumerate(ds.items()):
+#     for fold_index, (train, test) in enumerate(skf.split(dataset, y)):
+#         for clf_index, clf_name in enumerate(clfs):
+#             clf = clone(clfs[clf_name])
+#             clf.fit(dataset[train], y[train])
+#             y_pred = clf.predict(dataset[test])
+#             scores[clf_index, idx, fold_index] = accuracy_score(
+#                 y[test], y_pred)
+
+scores = np.empty((len(clfs), folds))
+scores_pca = np.empty((len(clfs), folds))
+scores_skb = np.empty((len(clfs), folds))
+scores_anova = np.empty((len(clfs), folds))
+
+for fold_index, (train, test) in enumerate(skf.split(X, y)):
+    for clf_index, clf_name in enumerate(clfs):
+
+        X_train, X_test = X[train], X[test]
+        y_train, y_test = y[train], y[test]
+
+        # base
+
+        clf = clone(clfs[clf_name])
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+        scores[clf_index][fold_index] = accuracy_score(y_test, y_pred)
+
+        # pca
+
+        pca = PCA(n_components=3)
+        X_pca = pca.fit_transform(X)
+        X_pca_train, X_pca_test = X_pca[train], X_pca[test]
+
+        clf = clone(clfs[clf_name])
+        clf.fit(X_pca_train, y_train)
+        y_pred = clf.predict(X_pca_test)
+        scores_pca[clf_index][fold_index] = accuracy_score(y_test, y_pred)
+
+        # skb
+
+        skb = SelectKBest(f_classif, k=3)
+        X_skb = skb.fit_transform(X, y)
+        X_skb_train, X_skb_test = X_skb[train], X_skb[test]
+        # y_skb_train, y_skb_test = y_skb[train], y_skb[test]
+
+        clf = clone(clfs[clf_name])
+        clf.fit(X_skb_train, y_train)
+        y_pred = clf.predict(X_skb_test)
+        scores_skb[clf_index][fold_index] = accuracy_score(y_test, y_pred)
+
+        # anova
+
+        X_anova = AnovaFTransformer().fit_transform(X, y, n_features=3)
+        X_anova_train, X_anova_test = X_anova[train], X_anova[test]
+
+        clf = clone(clfs[clf_name])
+        clf.fit(X_anova_train, y_train)
+        y_pred = clf.predict(X_anova_test)
+        scores_anova[clf_index][fold_index] = accuracy_score(y_test, y_pred)
+
+print("BAZOWE: ")
+for clf_index, clf_name in (enumerate(clfs)):
+    print(clf_name + ': ' + str(np.mean(scores, axis=1)
+          [clf_index]) + ' ('+str(np.std(scores, axis=1)[clf_index]) + ')')
+print("PCA: ")
+for clf_index, clf_name in (enumerate(clfs)):
+    print(clf_name + ': ' + str(np.mean(scores_pca, axis=1)
+          [clf_index]) + ' ('+str(np.std(scores_pca, axis=1)[clf_index]) + ')')
+print("SkB: ")
+for clf_index, clf_name in (enumerate(clfs)):
+    print(clf_name + ': ' + str(np.mean(scores_skb, axis=1)
+          [clf_index]) + ' ('+str(np.std(scores_skb, axis=1)[clf_index]) + ')')
+print("ANOVA: ")
+for clf_index, clf_name in (enumerate(clfs)):
+    print(clf_name + ': ' + str(np.mean(scores_anova, axis=1)
+                                [clf_index]) + ' ('+str(np.std(scores_anova, axis=1)[clf_index]) + ')')
 
 
-anova = AnovaFTransformer()
-X_trans = anova.fit_transform(X, y, n_features=4)
+ranks = []
 
-skb = SelectKBest(f_classif, k=4)
-skb.fit(X, y)
-print("--------")
-print(skb.transform(X))
-print("--------")
-print(X_trans)
-print("--------")
-print(skb.scores_)
-print("--------")
-print(anova.scores_)
 
-print("--------")
-print(X.shape)
-print("--------")
-print(X_trans.shape)
+# fig = plt.figure(1, figsize=(4, 3))
+# plt.clf()
+# ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+# for name, label in [('A', 0), ('B', 1), ('C', 2)]:
+#     ax.text3D(X[y == label, 0].mean(),
+#               X[y == label, 1].mean() + 1.5,
+#               X[y == label, 2].mean(), name,
+#               horizontalalignment='center',
+#               bbox=dict(alpha=.5, edgecolor='w', facecolor='w'))
+# # Reorder the labels to have colors matching the cluster results
+# y = np.choose(y, [1, 2, 0]).astype(float)
+# ax.scatter(X_trans[:, 0], X_trans[:, 1], X_trans[:, 2], c=y, cmap=plt.cm.nipy_spectral,
+#            edgecolor='k')
+
+# ax.w_xaxis.set_ticklabels([])
+# ax.w_yaxis.set_ticklabels([])
+# ax.w_zaxis.set_ticklabels([])
+# plt.show()
