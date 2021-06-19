d8c5524cde8b1df5adce00432c3d2929dbde928b
RadoslawCzubak <radoslaw.marek.czubak@gmail.com>
2021-06-19 13:46:52 +0200

    add method IN and mask for anova selector

--- AnovaSelector.py
+++ AnovaSelector.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class AnovaSelector(BaseEstimator, SelectorMixin):
     def _get_support_mask(self):
-        pass
+        return self.mask
 
     def __init__(self, k_features=5):
         self.k_features = k_features
@@ -15,12 +15,8 @@ class AnovaSelector(BaseEstimator, SelectorMixin):
         for feature in X.T:
             f_scores.append(self.calculate_f_value(feature, y))
         self.scores_ = np.array(f_scores)
-
-        # get n columns with highest score
-        idx = np.argpartition(self.scores_, -self.k_features)[-self.k_features:]
-        indices = idx[np.argsort((-self.scores_)[idx])]
-        new_X = X[:, sorted(indices)]
-        return new_X
+        self.set_mask(X)
+        return self.get_best_columns(X)
 
     @staticmethod
     def calculate_f_value(X, y=None):
@@ -45,3 +41,16 @@ class AnovaSelector(BaseEstimator, SelectorMixin):
 
     def get_scores(self):
         return self.scores_
+
+    def get_best_columns(self, X):
+        idx = np.argpartition(self.scores_, -self.k_features)[-self.k_features:]
+        indices = idx[np.argsort((-self.scores_)[idx])]
+        best_cols = X[:, sorted(indices)]
+        return best_cols
+
+    def set_mask(self, X):
+        indices = np.argpartition(self.scores_, -self.k_features)[-self.k_features:]
+        self.mask = np.zeros(X.shape[1], dtype=bool)
+        for index in range(len(self.mask)):
+            if index in indices:
+                self.mask[index] = True
